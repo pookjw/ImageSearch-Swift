87586af31cf8557ccd2a22cea944cf7e52274015
Jinwoo Kim <kidjinwoo@me.com>
2020-06-11 11:06:38 +0900

    Initial Commit

---
+++ ImageSearch/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ImageSearch
+//
+//  Created by pook on 6/11/20.
+//  Copyright © 2020 jinwoopeter. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
