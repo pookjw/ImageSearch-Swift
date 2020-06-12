d2761b73634ab9e7920d751084a9b98eeded1f6a
Jinwoo Kim <kidjinwoo@me.com>
2020-06-12 23:39:54 +0900

    FavoirtesViewController의 기본 틀 구성

---
+++ ImageSearch/Views/FavoirtesViewController.swift
@@ -0,0 +1,28 @@
+//
+//  FavoirtesViewController.swift
+//  ImageSearch
+//
+//  Created by pook on 6/11/20.
+//  Copyright © 2020 jinwoopeter. All rights reserved.
+//
+
+import UIKit
+
+class FavoirtesViewController: ImageBaseViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        super.cv = self.collectionView
+        // Do any additional setup after loading the view.
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+        super.imageInfo = ImageInfo.getSampleImageInfo()
+        DispatchQueue.main.async {
+            self.collectionView.reloadData()
+        }
+        InfoView.showIn(viewController: self, message: "Welcome!")
+    }
+}
+
